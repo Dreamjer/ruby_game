9f4315dd7b77345774dd6f5b8d671303a8df1634
Jerome Tessier <jerome.tessier@wandis.com>
2015-03-24 15:26:23 +0100

    Ajout image background

--- lib/ruby_game/game.rb
+++ lib/ruby_game/game.rb
@@ -2,8 +2,13 @@ module RubyGame
 	class Game < Gosu::Window
 		def initialize
 			super(640,480,false)
+			@background_image = Gosu::Image.new(self, File.join(IMAGES_PATH,'background.png'),true)
 		end
-		
+
+		def draw
+			@background_image.draw(0,0,0)
+		end
+
 		def start!
 			self.show
 		end
