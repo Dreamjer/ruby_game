8e0a18f1779ce26efb5fb227947ad21a132ed199
Jerome Tessier <jerome.tessier@wandis.com>
2015-03-24 18:52:04 +0100

    Deplacement player avec fleche + limites

--- lib/ruby_game/game.rb
+++ lib/ruby_game/game.rb
@@ -4,17 +4,24 @@ module RubyGame
 			super(640,480,false)
 			@background_image = Gosu::Image.new(self, File.join(IMAGES_PATH,'background.png'),true)
 			#@diamant = Gosu::Image.new(self, File.join(IMAGES_PATH,'ruby.png'),true)
-			@diamant = Diamant.new(self,rand(0..640),rand(0..480))
+			@diamant = Diamant.new(self,rand(50..600),rand(50..400))
 			@player = Player.new(self,350,280)
 
 			#@player_image = Gosu::Image.new(self,File.join(IMAGES_PATH,'player.png'),true)
 			#@mario_image = Gosu::Image.new(self,File.join(IMAGES_PATH,'mario.png'),true)
 		end
 
+		def update
+			@player.move_left  if button_down?(Gosu::Button::KbLeft)
+			@player.move_right if button_down?(Gosu::Button::KbRight)
+			@player.move_up    if button_down?(Gosu::Button::KbUp)
+			@player.move_down  if button_down?(Gosu::Button::KbDown)
+		end
+
 		def draw
 			@background_image.draw(0,0,0)
-			@player.draw
-			@diamant.draw
+			[@player,@diamant].each { |obj| obj.draw}
+
 			#@mario_image.draw(45,45,0)
 		end
 
--- lib/ruby_game/player.rb
+++ lib/ruby_game/player.rb
@@ -3,5 +3,20 @@ module RubyGame
     def initialize(window,line,col, img = 'player.png')
       super
     end
+    @@bord_right=600
+    @@bord_bas=420
+
+    def move_left
+      @line-=3 if @line >= 0
+    end
+    def move_right
+      @line+=3 if @line <= @@bord_right
+    end
+    def move_up
+      @col-=3 if @col >=0
+    end
+    def move_down
+      @col+=3 if @col <= @@bord_bas
+    end
   end
 end
